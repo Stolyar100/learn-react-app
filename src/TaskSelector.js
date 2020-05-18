01b8178640c243da2649dfd25a57f5f25f4b24e8
Stolyar100 <olejer555@gmail.com>
2020-05-18 18:25:31 +0300

    Added component to choose showing task

---
+++ src/TaskSelector.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import Task1 from './components/task1/Task1'
+import ComponentA from './components/task2/ComponentA'
+
+export default class TaskSelector extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            value: '0',
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+    
+    render() {
+        let currentComponent = <></>;
+        switch (this.state.value) {
+            case '0':
+                currentComponent = <h3>Choose task</h3>;
+                break;
+            case '1':
+                currentComponent = <Task1 />;
+                break;
+            case '2':
+                currentComponent = <ComponentA />;
+                break;
+            default:
+                currentComponent = <h3>Something wentWrong</h3>;
+                break;
+        }
+        
+        return (
+            <div>
+                <select 
+                value={this.state.value}
+                onChange={this.handleChange}>
+                    <option value="0" disabled>Choose Task</option>
+                    <option value="1">Task1</option>
+                    <option value="2">Task2</option>
+                </select>
+                {currentComponent}
+            </div>
+        )
+    }
+}
