36ed62350c7c08caa7efbe8209d07d25551e0d4a
Stolyar100 <olejer555@gmail.com>
2020-05-19 16:25:30 +0300

    added layout for todo

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Todo extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: '',
+        };
+    }
+
     render() {
         return (
-            <div>
-                Todo
+            <div className="todo">
+                <input
+                    className="todo__input"
+                    type="text"
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                />
+                <div className="todo__list"></div>
             </div>
-        )
+        );
     }
 }
