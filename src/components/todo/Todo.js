a891b3315c5afad1ff22737aa3f56995ce238760
Stolyar100 <olejer555@gmail.com>
2020-05-19 16:39:46 +0300

    changed layout, added events handlers

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -7,17 +7,30 @@ export default class Todo extends Component {
         this.state = {
             value: '',
         };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value});
+    }
+
+    handleClick() {
+
     }
 
     render() {
         return (
             <div className="todo">
-                <input
+                <div className="todo__add-block">
+                    <input
                     className="todo__input"
                     type="text"
                     value={this.state.value}
                     onChange={this.handleChange}
-                />
+                    />
+                    <button className="todo__add-btn" />
+                </div>
                 <div className="todo__list"></div>
             </div>
         );
