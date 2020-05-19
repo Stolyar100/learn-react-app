664d20ee5a2d4ffa078563866ad5ce9adb0d7020
Stolyar100 <olejer555@gmail.com>
2020-05-19 22:16:44 +0300

    added to state todos array, functions to add new todo item to todos list, map it to render todolist

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import TodoItem from './TodoItem';
 
-import './todo.css';
+import './Todo.css';
+import './TodoItem.css';
 
 export default class Todo extends Component {
     constructor(props) {
@@ -8,33 +10,72 @@ export default class Todo extends Component {
 
         this.state = {
             value: '',
+            idForTodo: '4',
+            todos: [
+                {
+                    id: 0,
+                    title: 'Sit at the bottom of apple tree',
+                },
+                {
+                    id: 1,
+                    title: 'Avoid getting hit by falling apple',
+                },
+                {
+                    id: 2,
+                    title: '???',
+                },
+                {
+                    id: 3,
+                    title: 'Explain gravity',
+                },
+            ],
         };
         this.handleChange = this.handleChange.bind(this);
-        this.handleClick = this.handleClick.bind(this);
+        this.addTodo = this.addTodo.bind(this);
     }
 
     handleChange(event) {
-        this.setState({ value: event.target.value});
+        this.setState({ value: event.target.value });
     }
 
-    handleClick() {
+    addTodo() {
+        if (this.state.value.trim().length === 0) {
+            this.setState({ value: '' });
+            return;
+        }
 
+        this.setState((state) => ({
+            todos: [
+                ...state.todos,
+                {
+                    id: state.idForTodo,
+                    title: state.value,
+                },
+            ],
+            value: '',
+            idForTodo: state.idForTodo++,
+        }));
     }
 
     render() {
+        const todoList = this.state.todos.map((todo) => (
+            <TodoItem title={todo.title} key={todo.id} />
+        ));
         return (
             <div className="todo">
                 <div className="todo__add-block">
                     <input
-                    className="todo__input"
-                    type="text"
-                    placeholder="enter task"
-                    value={this.state.value}
-                    onChange={this.handleChange}
+                        className="todo__input"
+                        type="text"
+                        placeholder="enter task"
+                        value={this.state.value}
+                        onChange={this.handleChange}
                     />
-                    <button className="todo__add-btn">add</button>
+                    <button className="todo__add-btn" onClick={this.addTodo}>
+                        add
+                    </button>
                 </div>
-                <div className="todo__list"></div>
+                <div className="todo__list">{todoList}</div>
             </div>
         );
     }
