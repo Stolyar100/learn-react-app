73aef8d27a55791ada2f3fed83076ca1f1ba7218
Stolyar100 <olejer555@gmail.com>
2020-05-20 19:11:48 +0300

    finished Form styling

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
-import './Form.css'
+import './Form.css';
 
 export default function Form() {
     return (
-        <form className="form">
-            <input className="form__email"/>
-            <input className="form__password"/>
-            <input className="form__checkbox"/>
-            <button className="form_sign-btn">Sign in</button>
-            <button className="form_forgot-btn">Forgot your password?</button>
-        </form>
-    )
+        <div className="form">
+            <form className="form__block">
+                <h3 className="form__title">Sign in to your account</h3>
+                <input type="email" className="form__input form__email" />
+                <input type="password" className="form__input form__password" />
+                <div className="form__checkbox-block">
+                    <input className="form__checkbox" type="checkbox" />
+                    <p className="form__description">Keep me signed in</p>
+                </div>
+                <button className="form_sign-btn">Sign in</button>
+                <button className="form__description form_forgot-btn">
+                    Forgot your password?
+                </button>
+            </form>
+        </div>
+    );
 }
