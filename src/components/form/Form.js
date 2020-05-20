2232b4fc9707788a0a0a1d77c582ea23eebc6d65
Stolyar100 <olejer555@gmail.com>
2020-05-20 12:07:04 +0300

    added form layout

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function Form() {
     return (
-        <div>
-            form
-        </div>
+        <form className="form">
+            <input className="form__email"/>
+            <input className="form__password"/>
+            <input className="form__checkbox"/>
+            <button className="form_sign-btn">Sign in</button>
+            <button className="form_forgot-btn">Forgot your password?</button>
+        </form>
     )
 }
