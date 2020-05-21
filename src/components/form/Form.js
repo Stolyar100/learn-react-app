e0784cf3e9ad90fe3b090e52eca670c7ded58558
Stolyar100 <olejer555@gmail.com>
2020-05-21 12:44:45 +0300

    added form validation

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import { withFormik, Form, Field } from 'formik';
-import Yup from 'yup';
+import * as Yup from 'yup';
 
 import './Form.css';
 
-const FormLayout = ({ values }) => {
+const FormLayout = ({ values, errors, touched }) => {
+    const emailError = <div className="form__error">{errors.email}</div>;
+    const passwordError = <div className="form__error">{errors.password}</div>;
+    const KeepSignInError = <div className="form__error">{errors.keepSignIn}</div>;
     return (
         <div className="form">
             <Form className="form__block">
                 <h3 className="form__title">Sign in to your account</h3>
+                {touched.email && errors.email && emailError}
                 <Field
                     type="email"
                     name="email"
                     className="form__input form__email"
                     placeholder="email"
                 />
+                {touched.password && errors.password && passwordError}
                 <Field
                     type="password"
                     name="password"
                     className="form__input form__password"
                     placeholder="password"
                 />
+                {touched.keepSignIn && errors.keepSignIn && KeepSignInError}
                 <label className="form__checkbox-block">
                     <Field
                         type="checkbox"
@@ -46,6 +52,11 @@ export const SignInForm = withFormik({
             keepSignIn: false,
         };
     },
+    validationSchema: Yup.object().shape({
+        email: Yup.string().email().required('email is required'),
+        password: Yup.string().min(8).required('password is required'),
+        keepSignIn: Yup.boolean().oneOf([true], 'checkbox is required'),
+    }),
     handleSubmit(values) {
         console.log(values);
     },
